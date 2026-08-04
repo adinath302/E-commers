3c1cd8700d7b6fd820ebd9c1c9c8a4ca7a427259
Adinath <adinathgaware23072003@gmail.com>
2026-08-04 22:32:48 +0530

    w

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
       <Navbar />
       <main>
         {/* Outlet is used to render the child routes */}
-        <Outlet /> 
+        <Outlet />
       </main>
     </div>
   );
