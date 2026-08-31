fdf4c5219f2c6c39077d060d265a05f9f6031e03
Adinath <adinathgaware23072003@gmail.com>
2026-08-31 23:03:41 +0530

    w

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -2,8 +2,10 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuthStore from "../store/useAuthStore";
 
 const ProtectedRoute = () => {
+  // it takes  the access token form useAuthStore
   const accessToken = useAuthStore((state) => state.accessToken);
   
+  // if the user has token or not
   if (!accessToken) {
     return <Navigate to="/login" replace />;
   }
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -4,10 +4,7 @@ interface AuthState {
   accessToken: string | null;
   refreshToken: string | null;
 
-  setTokens: (
-    accessToken: string,
-    refreshToken: string
-  ) => void;
+  setTokens: (accessToken: string, refreshToken: string) => void;
 
   logout: () => void;
 }
